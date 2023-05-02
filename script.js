b3e7a07372b361d9aea367bf0f2b802172f8ed01
Ng Geok Theng <gthengng@gmail.com>
2023-05-02 17:14:17 +0800

    Modify the gameBoard and add in draw functionality

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-const displayController = (() => {
-    const startBtn = document.getElementById('start-btn');
-    const restartBtn = document.getElementById('restart-btn');
-    const board = document.querySelector('.board');
-
-    // start game
-    startBtn.addEventListener('click', startGameDisplay);
-
-    function startGameDisplay() {
-        board.style.display = 'grid';
-        startBtn.style.display = 'none';
-        restartBtn.style.display = 'block';
-
-        // make the players' name input hidden once the players press start
-        const player1_div = document.querySelector('.player1');
-        const player2_div = document.querySelector('.player2');
-        player1_div.style.display = 'none';
-        player2_div.style.display = 'none';
-
-        // store the players' name in object
-        const player1 = document.getElementById('player1').value;
-        const player2 = document.getElementById('player2').value;
-
-        // display players' name on the browser
-        const displayMessage = document.getElementById('display-message-element');
-        displayMessage.innerText = `${player1} (O) vs ${player2} (X)`;
-    }
-})();
-
 const gameBoard = (() => {
+    const displayMessageElement = document.getElementById('display-message-element');
+    const board = document.getElementById('board');
     const cells = document.querySelectorAll('.cell');
-    const displayResult = document.getElementById('display-result-element');
+    const restartBtn = document.getElementById('restart-btn');
+
+    const CIRCLE = 'circle';
+    const CROSS = 'cross';
     const winningCombinations = [
         [0, 1, 2],
         [3, 4, 5],
@@ -38,57 +14,73 @@ const gameBoard = (() => {
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 5, 8]
+        [2, 4, 6]
     ];
-    let turn = 'circle';
-    let round = 0;
+
+    let currentPlayer = CIRCLE;
+    let count = 0;
 
     cells.forEach(cell => cell.addEventListener('click', handleClick));
 
     function handleClick(e) {
+        const index = e.target.id;
         const selectedSpot = e.target;
-
-        // if existing spot is marked, that spot can't be clicked
-        if (selectedSpot.classList.contains('circle') || selectedSpot.classList.contains('cross')) {
+    
+        // can't mark the same spot
+        if (selectedSpot.classList.contains(CIRCLE) || selectedSpot.classList.contains(CROSS)) {
             return;
         }
-
+    
         // place mark
-        if (turn) {
-            selectedSpot.classList.add('circle');
-        } else {
-            selectedSpot.classList.add('cross');
+        if (!cells[index].classList.contains(currentPlayer) && count < 9) {
+            cells[index].classList.add(currentPlayer);
+            count++;
         }
-
+    
         // switch turn
-        switchTurn();
-
+        currentPlayer = currentPlayer === CIRCLE ? CROSS : CIRCLE;
+    
         // check for win
         checkResult();
-    }
-
-    function switchTurn() {
-        turn = !turn;
+    
+        // check for draw
+        if (count === 9 && displayMessageElement.innerText === 'START GAME') {
+            displayMessageElement.innerText = `It's a draw!`;
+        }
     }
 
     function checkResult() {        
         winningCombinations.forEach(combination => {
             const circleWins = combination.every(index => cells[index].classList.contains('circle'));
             const crossWins = combination.every(index => cells[index].classList.contains('cross'));
-
+    
             if (circleWins) {
-                displayResult.style.display = 'block';
-                displayResult.innerText = 'Circle wins!';
-                // remove addEventListener
-                cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+                displayMessageElement.innerText = 'Circle wins!';
                 return;
             } else if (crossWins) {
-                displayResult.style.display = 'block';
-                displayResult.innerText = 'Cross wins!';
-                // remove addEventListener
-                cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+                displayMessageElement.innerText = 'Cross wins!';
                 return;
             }
         });
+    }
+
+    restartBtn.addEventListener('click', restart)
+
+    function restart() {
+        cells.forEach(cell => {
+            cell.classList.remove(CIRCLE);
+            cell.classList.remove(CROSS);
+        });
+
+        displayMessageElement.innerText = 'START GAME';
+
+        currentPlayer = CIRCLE;
+        count = 0;
+    }
+
+    return {
+        handleClick,
+        checkResult,
+        restart
     }
 })();
